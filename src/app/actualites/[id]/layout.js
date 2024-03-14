9807c48f776b521313761b1dccb06ed67f9da8a9
Aurore Bartel <a.bartel@comsea.fr>
2024-03-14 11:39:12 +0100

    testage

--- src/app/actualites/[id]/layout.js
+++ src/app/actualites/[id]/layout.js
@@ -6,6 +6,7 @@ export async function generateMetadata(req, res) {
     return {
         openGraph: {
             images : imageUrl,
+            title: "Actus"
         },
     };
 }
--- src/app/api/opengraph/[id]/route.js
+++ src/app/api/opengraph/[id]/route.js
@@ -1,10 +1,12 @@
 export async function GET(req, res) {
+
     try {
-        const response = await fetch(`https://apitda.comsea.fr/api/actualites/${res.params.id}`)
-        let imageUrl = "https://testtda.comsea.fr/images/Footer/Logo-TDA.png"
-        if (1 === 1) {
-            imageUrl = "https://apitda.comsea.fr/build/images/634082665146a73a09d5a2c63141d3c155dad50f.png"
-        }
+        await fetch(`https://apitda.comsea.fr/api/actualites/${res.params.id}`)
+        .then((response) => response.json())
+        .then((result) => {
+            result;
+        })
+        imageUrl = `https://apitda.comsea.fr/build/images/${result.photo}`
         return new Response(imageUrl, { status: 200 })
     } catch (err) {
         console.log(err)
