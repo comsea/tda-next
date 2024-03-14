9e551bb664065c7dc677984160760f92f3354711
Aurore Bartel <a.bartel@comsea.fr>
2024-03-14 15:00:43 +0100

    add description

--- src/app/actualites/[id]/layout.js
+++ src/app/actualites/[id]/layout.js
@@ -2,14 +2,16 @@ export async function generateMetadata(req, res) {
     const id = req.params.id
     const responseIMG = await fetch(`https://testtda.comsea.fr/api/openGraph/actus/image/${id}`)
     const responseTitle = await fetch(`https://testtda.comsea.fr/api/openGraph/actus/titre/${id}`)
+    const responseDesc = await fetch(`https://testtda.comsea.fr/api/openGraph/actus/description/${id}`)
     const imageUrl = await responseIMG.text();
     const titleUrl = await responseTitle.text()
+    const descUrl = await responseDesc.text()
     return {
         openGraph: {
             images : imageUrl,
             url: `https://testtda.comsea.fr/actualites/${id}`,
             title: titleUrl,
-            description: 'description',
+            description: descUrl,
         },
     };
 }
