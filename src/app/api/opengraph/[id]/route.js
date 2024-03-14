43f87c853c6f07be588e0081e2fcb826be4d2db3
Aurore Bartel <a.bartel@comsea.fr>
2024-03-14 14:42:42 +0100

    envoie tableau

--- src/app/api/opengraph/[id]/route.js
+++ src/app/api/opengraph/[id]/route.js
@@ -6,14 +6,14 @@ export async function GET(req, res) {
         let imageUrl = `https://apitda.comsea.fr/build/images/${result.photo}`
         let titleUrl = result.title
         let descUrl = result.description
-        let data = {
-            imageUrl: imageUrl,
-            titleUrl: titleUrl,
-            descUrl: descUrl
-        }
+        let data = [
+            imageUrl,
+            titleUrl,
+            descUrl
+        ]
 
-        console.log(data.imageUrl)
-        return new Response(JSON.stringify(data), { 
+        console.log(data)
+        return new Response(data, { 
             status: 200,
             headers: {
                 'Content-Type': 'application/json'
