51a0ffb7fe50a3fa9cb1933b48d6d3bf3092355d
Comsea <contact@comsea.fr>
2024-04-30 10:32:16 +0200

    contain

--- src/components/MiniaSlideRea.jsx
+++ src/components/MiniaSlideRea.jsx
@@ -14,7 +14,7 @@ const MiniaSlideRea = ({ images }) => {
   return (
     <div className="flex flex-col lg:flex-row items-center lg:justify-between w-full">
       <div className="w-[60%] lg:h-[500px] h-auto">
-        <img src={mainImage} alt="Main Image" className="w-full h-full object-cover" />
+        <img src={mainImage} alt="Main Image" className="w-full h-full object-contain" />
       </div>
       <div className="w-[38%] grid grid-cols-2 gap-4 justify-center mt-4">
         {images.map((image, index) => (
@@ -22,7 +22,7 @@ const MiniaSlideRea = ({ images }) => {
             key={index}
             src={image}
             alt={`Thumbnail ${index + 1}`}
-            className={`lg:w-full w-12 object-cover h-auto mr-2 cursor-pointer ${selectedThumbnail === image ? 'outline outline-[#DF0624] outline-offset-2 outline-1' : ''}`}
+            className={`lg:w-full w-12 object-contain h-auto max-h-[200px] mr-2 cursor-pointer ${selectedThumbnail === image ? 'outline outline-[#DF0624] outline-offset-2 outline-1' : ''}`}
             onClick={() => handleThumbnailClick(image)}
           />
         ))}
