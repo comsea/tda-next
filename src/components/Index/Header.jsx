83ad72b3f12186b965eeee71d781ac3a76b17566
Comsea <contact@comsea.fr>
2024-05-07 15:48:59 +0200

    new new header

--- src/components/Index/Header.jsx
+++ src/components/Index/Header.jsx
@@ -47,10 +47,26 @@ export default function Header() {
         <div className="flex items-center justify-center h-full w-full bg-gray-200">
             <div className="relative w-full h-full">
                 <div className="absolute inset-0 flex items-center justify-center">
-                    {isLoading ? "Chargement en cours" : (
+                    {isLoading ?
+                        <div className='w-full h-full overflow-hidden'>
+                            <img src="/images/Accueil/bg.png" alt="" className='w-full h-full object-cover' />
+                                <div className="absolute inset-0 bg-black opacity-50"></div>
+                                <div className="absolute inset-0 flex items-center justify-center">
+                                    <div className="w-[90%] top-10 z-40 absolute">
+                                        <img src="/images/Footer/Logo-TDA.png" alt="Logo TDA blanc" className="lg:w-[100px] w-[50px]" />
+                                    </div>
+                                    <div className="w-[90%] h-full flex items-center justify-start">
+                                        <div className="text-white text-start flex flex-col justify-start items-start space-y-3">
+                                            <h2 className="lg:text-6xl text-4xl font-medium uppercase">Bienvenue sur le site de l'Agence TDA</h2>
+                                            <Link href={`/presentation`} className='bg-white text-[#DF0624] rounded-full lg:py-2 py-1 lg:px-6 px-4'>En savoir plus</Link>
+                                        </div>
+                                    </div>
+                            </div>
+                        </div>
+                    : (
                         <div className="relative w-full h-full overflow-hidden">
                             {realisations.slice(0,5).map((realisation, index) => (
-                            <div key={index} className={`absolute w-full h-full opacity-0 transition-opacity ${index === activeIndex? '!opacity-100' : ''}`}>
+                            <div key={index} className={`absolute w-full h-full z-0 transition-opacity ${index === activeIndex? '!z-10' : ''}`}>
                                 <img src={`${realisation.id === "id"? realisation.photo : "https://api.tda-archi.com/build/images/"+realisation.photo}`} alt="" className='w-full h-full object-cover' />
                                 <div className="absolute inset-0 bg-black opacity-50"></div>
                                 <div className="absolute inset-0 flex items-center justify-center">
@@ -60,7 +76,7 @@ export default function Header() {
                                     <div className="w-[90%] h-full flex items-center justify-start">
                                         <div className="text-white text-start flex flex-col justify-start items-start space-y-3">
                                             <h2 className="lg:text-6xl text-4xl font-medium uppercase">{realisation.title}</h2>
-                                            <Link href={`/realisations/categories/${realisation.id}`} className='bg-white text-[#DF0624] rounded-full lg:py-2 py-1 lg:px-6 px-4'>En savoir plus</Link>
+                                            <Link href={`${realisation.id === "id"? "/presentation" : "/realisations/categories/"+realisation.id}`} className='bg-white text-[#DF0624] rounded-full lg:py-2 py-1 lg:px-6 px-4'>En savoir plus</Link>
                                         </div>
                                     </div>
                                 </div>
